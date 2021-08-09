0155e579a3ba407f79d57e33bd6bba8bcc346f5c
isaskou <isabel.skou@yahoo.fr>
2021-08-09 16:38:36 +0200

    add default datas for product, categoryProduct + Roles

---
+++ BlackGarlic.DB/Script/Script.PostDeploymentGeneral.sql
@@ -0,0 +1,40 @@
+﻿/*
+Modèle de script de post-déploiement							
+--------------------------------------------------------------------------------------
+ Ce fichier contient des instructions SQL qui seront ajoutées au script de compilation.		
+ Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de post-déploiement.			
+ Exemple :      :r .\monfichier.sql								
+ Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de post-déploiement.		
+ Exemple :      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--:r .\Script.AddDefaultCategoryProduct.sql
+
+--:r .\Script.AddDefaultProduct.sql
+
+--:r .\Script.AddDefaultRoles.sql
+
+EXEC SP_AddCategoryProduct
+    @Name='apéritif',
+    @Description='on y retrouve des tapenades, des humus, des crèmes, etc.'
+    
+GO
+
+EXEC SP_AddProduct
+	@Name = 'Tapenade olives et ail noir', 
+    @Description = null,
+	@IsDisabled = 0,
+	@UnitPrice = 9.99, 
+	@CategoryProductId=1,
+    @ProviderId = null,
+	@PictureUrl =null
+GO
+
+INSERT INTO dbo.Role
+VALUES
+    ('Admin'), 
+    ('Client'),
+    ('Assistant'),
+    ('Com')
+GO
