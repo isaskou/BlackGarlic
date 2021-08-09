e5ba68a5edb0a3fa2fd19a1de6d4989e341ef522
isaskou <isabel.skou@yahoo.fr>
2021-08-09 17:01:27 +0200

    add ConnectionTool

---
+++ ConnectionTool/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionTool
+{
+    public class Command
+    {
+        public string Query { get; set; }
+        public bool IsStoredProcedure { get; set; }
+        public Dictionary<string,object> Parameters { get; set; }
+
+        public Command(string query, bool isStoredProcedure)
+        {
+            Query = query;
+            IsStoredProcedure = isStoredProcedure;
+            Parameters = new Dictionary<string, object>();
+        }
+
+        public void AddParameters(string parameterName, object value)
+        {
+            Parameters.Add(parameterName, value ?? DBNull.Value);
+        }
+    }
+}
