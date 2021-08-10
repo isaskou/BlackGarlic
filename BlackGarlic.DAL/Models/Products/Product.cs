9782dde91086d7718d80b2a6d84f03ca5175f52e
isaskou <isabel.skou@yahoo.fr>
2021-08-10 13:29:42 +0200

    update ProductModel

--- BlackGarlic.DAL/Models/Products/Product.cs
+++ BlackGarlic.DAL/Models/Products/Product.cs
@@ -15,9 +15,7 @@ namespace BlackGarlic.DAL.Models.Products
         public bool IsDisabled { get; set; }
         public double UnitPrice { get; set; }
         public int CategoryProductId { get; set; }
-        public CategoryProduct CategoryProduct { get; set; }
         public int? ProviderId { get; set; }
-        public Provider Provider { get; set; }
         public string PictureUrl { get; set; }
     }
 }
