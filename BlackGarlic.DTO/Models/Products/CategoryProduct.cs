84f0a01148742f6b337b5421625766f17f1bd0c5
isaskou <isabel.skou@yahoo.fr>
2021-08-10 13:30:05 +0200

    add Products DTO's

---
+++ BlackGarlic.DTO/Models/Products/CategoryProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace BlackGarlic.DTO.Models.Products
+{
+    public class CategoryProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ BlackGarlic.DTO/Models/Products/Product.cs
@@ -0,0 +1,26 @@
+﻿using BlackGarlic.DTO.Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DTO.Models.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDisabled { get; set; }
+        public double UnitPrice { get; set; }
+        public List<CategoryProduct> Categories { get; set; }
+        public Provider Provider { get; set; }
+        public string PictureUrl { get; set; }
+
+        public Product()
+        {
+        }
+
+    }
+}
