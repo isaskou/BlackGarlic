1e719ede3a3530937a07cb9e376966fdcda660da
isaskou <isabel.skou@yahoo.fr>
2021-08-10 13:59:11 +0200

    create API Project

---
+++ BlackGarlic.API/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BlackGarlic.API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
