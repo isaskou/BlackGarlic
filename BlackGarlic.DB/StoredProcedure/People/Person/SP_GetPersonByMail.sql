8bec7d312f40639f5ee0c6ffac6652298cc9d6fd
isaskou <isabel.skou@yahoo.fr>
2021-08-12 16:11:24 +0200

    add SP for PersonTable

---
+++ BlackGarlic.DB/StoredProcedure/People/Person/SP_GetPersonByMail.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[SP_GetPersonByMail]
+	
+	@Email nvarchar(255)
+AS
+	SELECT p.Id, p.FirstName, p.LastName, p.Email, p.RoleId
+	FROM Person p
+	where Email=@Email
+	ORDER BY Email
+RETURN 0
---
+++ BlackGarlic.DB/StoredProcedure/People/Person/SP_GetPersonByName.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[SP_GetPersonByName]
+	@LastName nvarchar(50)
+AS
+	SELECT p.Id, p.FirstName, p.LastName, p.Email, p.RoleId
+	FROM Person p
+	Where LastName=@LastName
+	ORDER BY LastName
+RETURN 0
