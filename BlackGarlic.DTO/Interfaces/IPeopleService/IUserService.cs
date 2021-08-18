e03dcda3fc50ebb9ba6894cd51404a3eda7bf7d2
isaskou <isabel.skou@yahoo.fr>
2021-08-18 14:09:54 +0200

    add UserService

---
+++ BlackGarlic.DTO/Interfaces/IPeopleService/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using BlackGarlic.DTO.Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DTO.Interfaces.IPeopleService
+{
+    public interface IUserService
+    {
+        //Person GetUserWithAllInformationById(int id);
+
+        //Person GetClientByMailAndPasswordMatch(string email, string password);
+
+        //bool UpdateClientWithoutPassword(Person person);
+
+        bool FindEmail(string email, int idUser);
+    }
+}
---
+++ BlackGarlic.DTO/Services/PeopleService/UserService.cs
@@ -0,0 +1,41 @@
+﻿using BlackGarlic.DAL.Interfaces.IPeopleRepos;
+using BlackGarlic.DTO.Interfaces.IPeopleService;
+using BlackGarlic.DTO.Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DTO.Services.PeopleService
+{
+    public class UserService :  IUserService
+    {
+        private IUserRepository _repo;
+
+        public UserService(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public bool FindEmail(string email, int idUser)
+        {
+            return _repo.FindEmail(email, idUser);
+        }
+
+        //public Person GetClientByMailAndPasswordMatch(string email, string password)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Person GetUserWithAllInformationById(int id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public bool UpdateClientWithoutPassword(Person person)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
