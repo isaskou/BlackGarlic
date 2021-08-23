c1ddd69c4c821bdd86d51380715539b8cc73181b
isaskou <isabel.skou@yahoo.fr>
2021-08-23 14:51:35 +0200

    add ICustomerAdressRepo + CustomerRepo

---
+++ BlackGarlic.DAL/Interfaces/IPeopleRepos/ICustomerAdressRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlackGarlic.DAL.Interfaces.IRepo;
+using BlackGarlic.DAL.Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DAL.Interfaces.IPeopleRepos
+{
+    public interface ICustomerAdressRepository : IRepository<CustomerAdress, int>
+    {
+    }
+}
---
+++ BlackGarlic.DAL/Repositories/People/CustomerAdressRepository.cs
@@ -0,0 +1,53 @@
+﻿using BlackGarlic.DAL.Interfaces.IPeopleRepos;
+using BlackGarlic.DAL.Models.People;
+using BlackGarlic.DAL.Repositories.Base;
+using ConnectionTool;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DAL.Repositories.People
+{
+    public class CustomerAdressRepository : BaseRepository, ICustomerAdressRepository
+    {
+        public CustomerAdressRepository(IConfiguration config) : base(config)
+        {
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CustomerAdress> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerAdress GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(CustomerAdress entity)
+        {
+            Command cmd = new Command("SP_AddCustomerAdress", true);
+            cmd.AddParameters("CustomerId", entity.CustomerId);
+            cmd.AddParameters("AdressId", entity.AdressId);
+
+            return (int)_connection.ExecuteScalar(cmd);
+        }
+
+        public void Update(CustomerAdress entity)
+        {
+            Command cmd = new Command("SP_UpdateCustomerAdress", true);
+            cmd.AddParameters("CustomerId", entity.CustomerId);
+            cmd.AddParameters("AdressId", entity.AdressId);
+
+            _connection.ExecuteNonQuery(cmd);
+        }
+    }
+}
