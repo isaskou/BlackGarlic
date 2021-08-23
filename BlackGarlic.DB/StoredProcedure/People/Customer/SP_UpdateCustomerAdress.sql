bc6a85fbfe6523e8f06ec5ab13a93afd6c8f2902
isaskou <isabel.skou@yahoo.fr>
2021-08-23 14:50:45 +0200

    add SP_UpdateCustomerAdress

---
+++ BlackGarlic.DB/StoredProcedure/People/Customer/SP_UpdateCustomerAdress.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[SP_UpdateCustomerAdress]
+	@CustomerAdressId int,
+	@CustomerId int, 
+	@AdressId int
+AS
+	BEGIN
+		UPDATE CustomersAdresses
+		SET CustomerId=@CustomerId, AdressId=@AdressId
+		WHERE Id=@CustomerAdressId
+	END
