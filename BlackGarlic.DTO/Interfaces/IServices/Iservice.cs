a15596198aa66b8a8b50c59a357d3850cef6cbbe
isaskou <isabel.skou@yahoo.fr>
2021-09-01 11:32:43 +0200

    update IService to have more Interfaces

--- BlackGarlic.DTO/Interfaces/IServices/Iservice.cs
+++ BlackGarlic.DTO/Interfaces/IServices/Iservice.cs
@@ -6,12 +6,33 @@ using System.Threading.Tasks;
 
 namespace BlackGarlic.DTO.Interfaces.IServices
 {
-    public interface IService<T> where T : class
+    public interface IService<T> : IGetAllService<T>, IGetOneService<T>, IInsertService<T>, IUpdateService<T>, IDeleteService<T>
+    {
+    }
+
+    public interface IGetAllService<T>
     {
         IEnumerable<T> GetAll();
-        T GetById(int id);
-        int Insert(T entity);
-        void Update(T entity);
-        bool Delete(int id);
     }
+
+    public interface IGetOneService<T>
+    {
+        T GetOne(int Id);
+    }
+
+    public interface IInsertService<T>
+    {
+        int Insert(T Value);
+    }
+
+    public interface IUpdateService<T>
+    {
+        bool Update(T Value);
+    }
+
+    public interface IDeleteService<T>
+    {
+        bool Delete(int Id);
+    }
+
 }
