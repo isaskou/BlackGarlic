7d21261dfc72fedce06d063b2c27d9e506cf1d98
isaskou <isabel.skou@yahoo.fr>
2021-09-01 17:37:32 +0200

    add Role Repo & Mapper

---
+++ BlackGarlic.DAL/Repositories/People/RoleRepository.cs
@@ -0,0 +1,33 @@
+﻿using BlackGarlic.DAL.Interfaces.IPeopleRepos;
+using BlackGarlic.DAL.Models.People;
+using BlackGarlic.DAL.Repositories.Base;
+using BlackGarlic.DAL.Tools;
+using ConnectionTool;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DAL.Repositories.People
+{
+    public class RoleRepository : BaseRepository, IRoleRepository
+    {
+        public RoleRepository(IConfiguration config) : base(config)
+        {
+        }
+
+        public IEnumerable<Role> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Role GetOne(int id)
+        {
+            Command cmd = new Command("SP_GetRoleById", true);
+            cmd.AddParameters("@Id", id);
+            return _connection.ExecuteReader(cmd, Converter.RoleConvert).SingleOrDefault();
+        }
+    }
+}
---
+++ BlackGarlic.DTO/Tools/PeopleMapper/RoleMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dto = BlackGarlic.DTO.Models.People;
+using dal = BlackGarlic.DAL.Models.People;
+
+namespace BlackGarlic.DTO.Tools.PeopleMapper
+{
+    public static class RoleMapper
+    {
+        public static dto.Role toDAL(this dal.Role r)
+        {
+            return new dto.Role()
+            {
+                Id = r.Id,
+                Name = r.Name
+            };
+        }
+
+        public static dal.Role toDTO(this dto.Role r)
+        {
+            return new dal.Role()
+            {
+                Id = r.Id,
+                Name = r.Name
+            };
+        }
+    }
+}
