e0d1fe9343cbd97a78e1be03105545b5936d5c39
isaskou <isabel.skou@yahoo.fr>
2021-09-01 11:21:18 +0200

    update DTOModels for Command+CommandLine

---
+++ BlackGarlic.DTO/Models/Caddies/Command.cs
@@ -0,0 +1,33 @@
+﻿using BlackGarlic.DTO.Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DTO.Models.Caddies
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        public DateTime? DateOfCommand { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public int CustommerAdressId { get; set; }
+        public CustomerAdress CustomerAdress { get; set; }
+        public List<CommandProduct> CommandProducts { get; set; }
+
+        public Command()
+        {
+        }
+
+        public Command(int id, DateTime? dateOfCommand, int customerId, int custommerAdressId, List<CommandProduct> commandProducts)
+        {
+            Id = id;
+            DateOfCommand = dateOfCommand;
+            CustomerId = customerId;
+            CustommerAdressId = custommerAdressId;
+            CommandProducts = commandProducts;
+        }
+    }
+}
---
+++ BlackGarlic.DTO/Models/Caddies/CommandProduct.cs
@@ -0,0 +1,21 @@
+﻿using BlackGarlic.DTO.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DTO.Models.Caddies
+{
+    public class CommandProduct
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+        public int CommandId { get; set; }
+        public Command Command { get; set; }
+
+    }
+}
