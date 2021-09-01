930d1d67a891bad7c65a841de5921865555bace7
isaskou <isabel.skou@yahoo.fr>
2021-09-01 11:34:18 +0200

    add ICommandService

---
+++ BlackGarlic.DTO/Interfaces/ICaddieServices/ICommandService.cs
@@ -0,0 +1,14 @@
+﻿using BlackGarlic.DTO.Interfaces.IServices;
+using BlackGarlic.DTO.Models.Caddies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DTO.Interfaces.ICaddieServices
+{
+    public interface ICommandService : IInsertService<Command>, IGetOneService<CommandDetail>, IGetAllService<Command>
+    {
+    }
+}
