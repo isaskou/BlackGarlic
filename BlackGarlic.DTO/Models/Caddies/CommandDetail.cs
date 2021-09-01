16555832e8e7e0197f16b9ea05249fde62b9f635
isaskou <isabel.skou@yahoo.fr>
2021-09-01 11:31:20 +0200

    update CommandProduct + add CommandDetail

---
+++ BlackGarlic.DTO/Models/Caddies/CommandDetail.cs
@@ -0,0 +1,18 @@
+﻿using BlackGarlic.DTO.Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackGarlic.DTO.Models.Caddies
+{
+    public class CommandDetail
+    {
+        public int Id { get; set; }
+        public DateTime? DateOfCommand { get; set; }
+        public Customer Customer { get; set; }
+        public Adress Adress { get; set; }
+        public List<CommandProduct> CommandProducts { get; set; }
+    }
+}
--- BlackGarlic.DTO/Models/Caddies/CommandProduct.cs
+++ BlackGarlic.DTO/Models/Caddies/CommandProduct.cs
@@ -12,10 +12,9 @@ namespace BlackGarlic.DTO.Models.Caddies
         public int Id { get; set; }
         public int Quantity { get; set; }
         public int ProductId { get; set; }
-
-        public Product Product { get; set; }
         public int CommandId { get; set; }
-        public Command Command { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
 
     }
 }
