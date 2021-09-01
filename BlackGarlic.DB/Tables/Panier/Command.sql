86d1a5c30b7ee934cd176724371e5da694b04f9d
isaskou <isabel.skou@yahoo.fr>
2021-09-01 10:09:33 +0200

    rename table Caddie+CaddieLine in Command+CommandLine & update table

---
+++ BlackGarlic.DB/Tables/Panier/Command.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Command]
+(
+	[Id] INT NOT NULL IDENTITY,
+	DateOfCommand datetime NOT NULL,
+	CustomerId INT NOT NULL,
+	CustomerAdressId INT NOT NULL,
+
+	CONSTRAINT PK_Command PRIMARY KEY (Id),
+
+	CONSTRAINT FK_Customer_Command
+		FOREIGN KEY (CustomerId)
+		REFERENCES Customer(Id),
+
+	CONSTRAINT FK_CustomerAdress_Command
+		FOREIGN KEY (CustomerAdressId)
+		REFERENCES CustomersAdresses(Id)
+)
---
+++ BlackGarlic.DB/Tables/Panier/CommandProduct.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].CommandProduct
+(
+	[Id] INT NOT NULL IDENTITY,
+	Quantity INT NOT NULL DEFAULT 0,
+	ProductId INT NOT NULL,
+	CommandId INT NOT NULL
+
+	CONSTRAINT PK_CommandProduct PRIMARY KEY (Id),
+
+	CONSTRAINT FK_Product_CommandProduct
+		FOREIGN KEY (ProductId) 
+		REFERENCES Product(Id),
+
+	CONSTRAINT FK_Command_CommandProduct
+		FOREIGN KEY (CommandId)
+		REFERENCES Command(Id),
+
+	CONSTRAINT CK_QuantityUpToZero
+		CHECK (Quantity>=0)
+)
