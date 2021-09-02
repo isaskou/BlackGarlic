430114e06921a5211c91b9faa17030098a8375a9
isaskou <isabel.skou@yahoo.fr>
2021-09-02 12:27:53 +0200

    rename folder + delete classMapper because add specific folder

---
+++ BlackGarlic.DTO/Mapper/PeopleMapper/AdressMapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dto = BlackGarlic.DTO.Models.People;
+using dal = BlackGarlic.DAL.Models.People;
+
+namespace BlackGarlic.DTO.Tools.PeopleMapper
+{
+    public static class AdressMapper
+    {
+        public static dto.Adress toDAL(this dal.Adress a)
+        {
+            return new dto.Adress()
+            {
+                Id = a.Id,
+                Street = a.Street,
+                Number = a.Num,
+                PostalCode = a.PostalCode,
+                City = a.City,
+                District = a.District
+            };
+        }
+
+        public static dal.Adress toDTO(this dto.Adress a)
+        {
+            return new dal.Adress
+            {
+                Id = a.Id,
+                Street = a.Street,
+                Num = a.Number,
+                PostalCode = a.PostalCode,
+                City = a.City,
+                District = a.District
+            };
+        }
+
+    }
+}
---
+++ BlackGarlic.DTO/Mapper/PeopleMapper/CustomerMapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dto = BlackGarlic.DTO.Models.People;
+using dal = BlackGarlic.DAL.Models.People;
+
+namespace BlackGarlic.DTO.Tools.PeopleMapper
+{
+    public static class CustomerMapper
+    {
+        public static dto.Customer toDAL(this dal.Customer c)
+        {
+            return new dto.Customer
+            {
+                Id = c.Id,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Phone = c.Phone,
+                Email = c.Email,
+                RoleId = c.RoleId,
+                CompanyName = c.CompanyName
+            };
+        }
+
+        public static dal.Customer toDTO(this dto.Customer c)
+        {
+            return new dal.Customer
+            {
+                Id = c.Id,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Phone = c.Phone,
+                Email = c.Email,
+                RoleId = c.RoleId,
+                CompanyName = c.CompanyName,
+                Password = c.Password
+            };
+
+
+        }
+    }
+}
